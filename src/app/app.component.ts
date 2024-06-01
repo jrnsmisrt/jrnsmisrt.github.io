fb328e5a25f4378109d947d00b92fe2b8c0c4cdb
jeroen <jeroen.smissaert@gmail.com>
2024-06-01 11:38:40 +0200

    Create project portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {WhoAmIComponent} from "./who-am-i/who-am-i.component";
+import {CurriculumComponent} from "./curriculum/curriculum.component";
+import {ContactComponent} from "./contact/contact.component";
+import {MatIcon} from "@angular/material/icon";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WhoAmIComponent, CurriculumComponent, ContactComponent, MatIcon, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
