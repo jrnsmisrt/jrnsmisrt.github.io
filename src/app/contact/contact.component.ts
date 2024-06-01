71b1d4c0e8285637e362ed2a22352ceb391af7cf
jeroen <jeroen.smissaert@gmail.com>
2024-06-01 20:38:43 +0200

    Add contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @Component({
   selector: 'js-contact',
   standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    MatFormField,
+    MatHint,
+    MatLabel,
+    MatInput
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
