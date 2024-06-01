9079fd8003424b2b7f87f063120746eedf99181c
jeroen <jeroen.smissaert@gmail.com>
2024-06-01 11:38:55 +0200

    Add components/design

---
+++ src/app/curriculum/curriculum.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'js-curriculum',
+  standalone: true,
+  imports: [],
+  templateUrl: './curriculum.component.html',
+  styleUrl: './curriculum.component.scss'
+})
+export class CurriculumComponent {
+
+}
---
+++ src/app/who-am-i/who-am-i.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'js-who-am-i',
+  standalone: true,
+  imports: [],
+  templateUrl: './who-am-i.component.html',
+  styleUrl: './who-am-i.component.scss'
+})
+export class WhoAmIComponent {
+
+}
