287aa6e5c731612f204d41915ccfd9b1a2f2e7aa
jeroen <jeroen.smissaert@gmail.com>
2024-06-01 16:29:05 +0200

    Refactor nav menu

---
+++ src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-nav-menu',
+  standalone: true,
+  imports: [
+    MatIcon
+  ],
+  templateUrl: './nav-menu.component.html',
+  styleUrl: './nav-menu.component.scss'
+})
+export class NavMenuComponent {
+
+}
