af1589108690d74964c6636ae6b6a729a5b7f528
jeroen <jeroen.smissaert@gmail.com>
2024-06-03 14:45:53 +0200

    Add media screen css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {WhoAmIComponent} from "./who-am-i/who-am-i.component";
 import {CurriculumComponent} from "./curriculum/curriculum.component";
 import {ContactComponent} from "./contact/contact.component";
 import {MatIcon} from "@angular/material/icon";
 import {SplashComponent} from "./header/splash.component";
 import {NavMenuComponent} from "./nav-menu/nav-menu.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WhoAmIComponent, CurriculumComponent, ContactComponent, MatIcon, SplashComponent, NavMenuComponent],
+  imports: [
+    RouterOutlet,
+    WhoAmIComponent,
+    CurriculumComponent,
+    ContactComponent,
+    MatIcon,
+    SplashComponent,
+    NavMenuComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
 import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'js-contact',
@@ -11,11 +12,33 @@ import {MatInput} from "@angular/material/input";
     MatFormField,
     MatHint,
     MatLabel,
-    MatInput
+    MatInput,
+    ReactiveFormsModule
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm = new FormBuilder().group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    message: ['', [Validators.required, Validators.minLength(6)]],
+  })
+  constructor(private formBuilder: FormBuilder){
+  }
+
+  submit(){
+
+  }
+
+  get firstName(): string {
+    return this.contactForm.get('firstName')?.value!;
+  }
+  get lastName(): string {
+    return this.contactForm.get('lastName')?.value!;
+  }
+  get message(): string {
+    return this.contactForm.get('message')?.value!;
+  }
 
 }
