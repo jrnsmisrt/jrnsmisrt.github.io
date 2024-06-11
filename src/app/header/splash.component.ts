0399f9e84d7d5ac006ac211029dafab6f1749863
jeroen <jeroen.smissaert@gmail.com>
2024-06-11 13:39:03 +0200

    idunno...

--- src/app/header/splash.component.ts
+++ src/app/header/splash.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MatIcon} from "@angular/material/icon";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'js-splash'
 ,  standalone: true,
   imports: [
-    MatIcon
+    MatIcon,
+    NgOptimizedImage
   ],
   templateUrl: './splash.component.html',
   styleUrl: './splash.component.scss'
